14c2d63750ebe7a03a42ed069224f116446263ba
darekAsz <Burek2005>
2019-01-21 12:39:43 +0100

    zmieniona zmienna w petli (były 2 identyczne)

--- 2test.py
+++ 2test.py
@@ -9,7 +9,7 @@ import random
 
 states = ('Healthy', 'Fever')
 
-observations = ('1', '2', '3', '4','5','6')
+observations = ('1', '2', '3', '4', '5', '6')
 
 start_probability = {'Healthy': 0.6, 'Fever': 0.4}
 
@@ -19,8 +19,8 @@ transition_probability = {
 }
 
 emission_probability = {
-    'Healthy': {'1': 0.16, '2': 0.16, '3': 0.16, '4':0.16, '5':0.16, '6':0.16},
-    'Fever': {'1': 0.1, '2': 0.1, '3': 0.1, '4':0.1, '5':0.1, '6':0.5},
+    'Healthy': {'1': 0.16, '2': 0.16, '3': 0.16, '4': 0.16, '5': 0.16, '6': 0.16},
+    'Fever': {'1': 0.1, '2': 0.1, '3': 0.1, '4': 0.1, '5': 0.1, '6': 0.5},
 }
 
 
@@ -71,46 +71,44 @@ pi = convert_map_to_vector(start_probability, states_label_index)
 
 h = hmm.HMM(A, B, pi)
 
-#print " " * 7, " ".join(("%10s" % observations_index_label[i]) for i in observations_index)
-#for s in range(0, 2):
+# print " " * 7, " ".join(("%10s" % observations_index_label[i]) for i in observations_index)
+# for s in range(0, 2):
 #    print "%7s: " % states_index_label[s] + " ".join("%10s" % ("%f" % v) for v in V[s])
-#print '\nThe most possible states and probability are:'
+# print '\nThe most possible states and probability are:'
 
-#for s in ss:
+# for s in ss:
 #    print states_index_label[s],
-#print p
-
+# print p
 
 
 A = np.array([[0.5, 0.5], [0.5, 0.5]])
-B = np.array([[0.16, 0.16, 0.16,0.16,0.16,0.16],[0.16, 0.16, 0.16,0.16,0.16,0.16]])
+B = np.array([[0.16, 0.16, 0.16, 0.16, 0.16, 0.16], [0.16, 0.16, 0.16, 0.16, 0.16, 0.16]])
 pi = np.array([0.5, 0.5])
-
-
 
 # run a baum_welch_train
 
 
-#print observations_data
-#print states_data
-for i in range(200):
-	observations_data, states_data = h.simulate(100)
-	for i in range(100):
-		rand = random.random()
-		if rand <= 0.1:
-			observations_data[i]=0
-		elif rand <= 0.2:
-			observations_data[i]=1
-		elif rand <= 0.3:
-			observations_data[i]=2
-		elif rand <= 0.4:
-			observations_data[i]=3
-		elif rand <= 0.5:
-			observations_data[i]=4
-		elif rand <= 1.0:
-			observations_data[i]=5
-	#print observations_data
-	guess = hmm.HMM(A,B,pi)
-	A, B, pi = guess.baum_welch_train(observations_data)
+# print observations_data
+# print states_data
+for i in range(100):
+    observations_data, states_data = h.simulate(100)
+    for j in range(100):
+        print(j)
+        rand = random.random()
+        if rand <= 0.1:
+            observations_data[j] = 0
+        elif rand <= 0.15:
+            observations_data[j] = 1
+        elif rand <= 0.2:
+            observations_data[j] = 2
+        elif rand <= 0.4:
+            observations_data[j] = 3
+        elif rand <= 0.5:
+            observations_data[j] = 4
+        elif rand <= 1.0:
+            observations_data[j] = 5
+    # print observations_data
+    guess = hmm.HMM(A, B, pi)
+    A, B, pi = guess.baum_welch_train(observations_data)
 
-print B
+print(B)
--- hmm.py
+++ hmm.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class HMM:
     """
     Order 1 Hidden Markov Model
@@ -31,12 +32,12 @@ class HMM:
         N = self.A.shape[0]
         T = len(obs_seq)
 
-        F = np.zeros((N,T))
-        F[:,0] = self.pi * self.B[:, obs_seq[0]]
+        F = np.zeros((N, T))
+        F[:, 0] = self.pi * self.B[:, obs_seq[0]]
 
         for t in range(1, T):
             for n in range(N):
-                F[n,t] = np.dot(F[:,t-1], (self.A[:,n])) * self.B[n, obs_seq[t]]
+                F[n, t] = np.dot(F[:, t - 1], (self.A[:, n])) * self.B[n, obs_seq[t]]
 
         return F
 
@@ -44,34 +45,32 @@ class HMM:
         N = self.A.shape[0]
         T = len(obs_seq)
 
-        X = np.zeros((N,T))
-        X[:,-1:] = 1
+        X = np.zeros((N, T))
+        X[:, -1:] = 1
 
-        for t in reversed(range(T-1)):
+        for t in reversed(range(T - 1)):
             for n in range(N):
-                X[n,t] = np.sum(X[:,t+1] * self.A[n,:] * self.B[:, obs_seq[t+1]])
+                X[n, t] = np.sum(X[:, t + 1] * self.A[n, :] * self.B[:, obs_seq[t + 1]])
 
         return X
 
     def observation_prob(self, obs_seq):
         """ P( entire observation sequence | A, B, pi ) """
-        return np.sum(self._forward(obs_seq)[:,-1])
-
-    
+        return np.sum(self._forward(obs_seq)[:, -1])
 
     def simulate(self, T):
 
         def draw_from(probs):
-            return np.where(np.random.multinomial(1,probs) == 1)[0][0]
+            return np.where(np.random.multinomial(1, probs) == 1)[0][0]
 
         observations = np.zeros(T, dtype=int)
         states = np.zeros(T, dtype=int)
         states[0] = draw_from(self.pi)
-        observations[0] = draw_from(self.B[states[0],:])
+        observations[0] = draw_from(self.B[states[0], :])
         for t in range(1, T):
-            states[t] = draw_from(self.A[states[t-1],:])
-            observations[t] = draw_from(self.B[states[t],:])
-        return observations,states
+            states[t] = draw_from(self.A[states[t - 1], :])
+            observations[t] = draw_from(self.B[states[t], :])
+        return observations, states
 
     def baum_welch_train(self, observations, criterion=0.05):
         n_states = self.A.shape[0]
@@ -87,34 +86,33 @@ class HMM:
             # Initialize beta
             beta = self._backward(observations)
 
-            xi = np.zeros((n_states,n_states,n_samples-1))
-            for t in range(n_samples-1):
-                denom = np.dot(np.dot(alpha[:,t].T, self.A) * self.B[:,observations[t+1]].T, beta[:,t+1])
+            xi = np.zeros((n_states, n_states, n_samples - 1))
+            for t in range(n_samples - 1):
+                denom = np.dot(np.dot(alpha[:, t].T, self.A) * self.B[:, observations[t + 1]].T, beta[:, t + 1])
                 for i in range(n_states):
-                    numer = alpha[i,t] * self.A[i,:] * self.B[:,observations[t+1]].T * beta[:,t+1].T
-                    xi[i,:,t] = numer / denom
+                    numer = alpha[i, t] * self.A[i, :] * self.B[:, observations[t + 1]].T * beta[:, t + 1].T
+                    xi[i, :, t] = numer / denom
 
             # gamma_t(i) = P(q_t = S_i | O, hmm)
-            gamma = np.squeeze(np.sum(xi,axis=1))
+            gamma = np.squeeze(np.sum(xi, axis=1))
             # Need final gamma element for new B
-            prod =  (alpha[:,n_samples-1] * beta[:,n_samples-1]).reshape((-1,1))
-            gamma = np.hstack((gamma,  prod / np.sum(prod))) #append one more to gamma!!!
+            prod = (alpha[:, n_samples - 1] * beta[:, n_samples - 1]).reshape((-1, 1))
+            gamma = np.hstack((gamma, prod / np.sum(prod)))  # append one more to gamma!!!
 
-            newpi = gamma[:,0]
-            newA = np.sum(xi,2) / np.sum(gamma[:,:-1],axis=1).reshape((-1,1))
+            newpi = gamma[:, 0]
+            newA = np.sum(xi, 2) / np.sum(gamma[:, :-1], axis=1).reshape((-1, 1))
             newB = np.copy(self.B)
 
             num_levels = self.B.shape[1]
-            sumgamma = np.sum(gamma,axis=1)
+            sumgamma = np.sum(gamma, axis=1)
             for lev in range(num_levels):
                 mask = observations == lev
-                newB[:,lev] = np.sum(gamma[:,mask],axis=1) / sumgamma
+                newB[:, lev] = np.sum(gamma[:, mask], axis=1) / sumgamma
 
             if np.max(abs(self.pi - newpi)) < criterion and \
-                            np.max(abs(self.A - newA)) < criterion and \
-                            np.max(abs(self.B - newB)) < criterion:
+                    np.max(abs(self.A - newA)) < criterion and \
+                    np.max(abs(self.B - newB)) < criterion:
                 done = 1
 
-	    self.A[:],self.B[:],self.pi[:] = newA,newB,newpi
-	    return self.A, self.B, self.pi
-	    
+            self.A[:], self.B[:], self.pi[:] = newA, newB, newpi
+            return self.A, self.B, self.pi
